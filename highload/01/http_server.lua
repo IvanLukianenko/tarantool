681b8345ecdae620a63ced004141ac6d256b562f
IvanLukianenko <lukianenko.ia@phystech.edu>
2021-04-01 22:15:09 +0300

    fixed something

--- highload/01/http_server.lua
+++ highload/01/http_server.lua
@@ -1,17 +1,37 @@
 local fio = require('fio')
+local io = require('io')
+local yaml = require('yaml')
 
-local file = fio.open('config.yml')
+local file
+local conf_name
+
+repeat 
+    conf_name = io.read()
+    conf_name = tostring(conf_name)
+    file = fio.open(conf_name)
+    if file == nil then
+        print("No such config file")
+    end
+
+    start, end_ = conf_name:find(".yml")
+
+    if end_ ~= #conf_name or start == nil then
+        print("No yaml file")
+    end 
+until file ~= nil and end_ == #conf_name
 
 local raw_conf = file:read()
 
-local yaml = require('yaml')
-
 local conf = yaml.decode(raw_conf)
 
+if conf == nil then
+    print("some problems")
+end
+
+local http_client = require('http.client').new({max_connections = 1})
 
 local function handler()
-    http_client = require('http.client').new({max_connections = 1})
-    req = http_client:request('GET',"google.com")
+    req = http_client:request('GET',conf.proxy.url)
     return {
         status = req.status,
         reason = req.reason,
